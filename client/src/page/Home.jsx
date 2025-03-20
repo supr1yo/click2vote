dd65f2cccb441802f37a6caf207a4ed2af91bf7a
supr1yo <supriyo.developer@gmail.com>
2025-03-20 11:36:49 +0530

    feat: Landing page

--- client/src/page/Home.jsx
+++ client/src/page/Home.jsx
@@ -1,18 +1,17 @@
+import './Home.css';
 
 export default function Home() {
-  
   const redirect = () => {
-    window.location.href = 'nigga';
-  }
+    window.location.href = 'create';
+  };
 
   return (
-    <>
-      <div>
-        <h1>Click2Vote</h1>
+    <div className="home-container">
+      <div className="content-wrapper">
+        <h1 className="headline">Click2Vote</h1>
+        <p className="subheadline">Your voice matters. Start voting in a few clicks!</p>
+        <button className="cta-button" onClick={redirect}>Get Started</button>
       </div>
-      <p className="read-the-docs">
-        <button onClick={redirect}>Get started</button>
-      </p>
-    </>
-  )
+    </div>
+  );
 }
