0d85c57bfa73d32d63965af286a033b607c7a04e
supr1yo <supriyo.developer@gmail.com>
2025-04-03 00:08:32 +0530

    feat: add routes

--- server/src/app.js
+++ server/src/app.js
@@ -1,26 +1,29 @@
 import dotenv from 'dotenv';
 dotenv.config({ path: './env' });
-import express, { urlencoded } from 'express';
+import express from 'express';
 const app = express();
-const PORT = 3000 || process.env.PORT;
+const PORT = process.env.PORT || 3000;
 import connectDB from './database/database.js';
 import cors from 'cors';
+
 import createPoll from './routes/createPoll.js';
+import deletePoll from './routes/deletePoll.js';
+import editPoll from './routes/editPoll.js';
+import viewPoll from './routes/viewPoll.js';
+import votePoll from './routes/votePoll.js';
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
 }));
 app.use(express.json()); 
-app.use(urlencoded({ extended: true }));
-
-app.get('/', (req, res) => {
-    res.send({
-        message: 'Hello World!'
-    });
-});
+app.use(express.urlencoded({ extended: true }));
 
 app.use('/', createPoll);
+app.use('/', deletePoll);
+app.use('/', editPoll);
+app.use('/', viewPoll);
+app.use('/', votePoll);
 
 connectDB().then(() => {
     app.listen(PORT, () => {
