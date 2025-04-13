2e84d6946ac96b847e9b614fe686e3ab16016282
supr1yo <supriyo.developer@gmail.com>
2025-04-14 00:47:56 +0530

    change in conditons

--- server/src/routes/createPoll.js
+++ server/src/routes/createPoll.js
@@ -6,12 +6,12 @@ const router = Router();
 router.post('/v1/create', async (req, res) => {
     
     try {
-        const { id, title, options, createdAt, endAt } = req.body;
+        const { id, title, options, startTime, endTime } = req.body;
 
-        if (!id || !title || !Array.isArray(options) || !createdAt || !endAt) {
+        if (!id || !title || !Array.isArray(options) || !startTime || !endTime) {
             return res.status(400).json({ message: 'Badly formatted request.' });
         }
-        await Poll.create({ id, title, options, createdAt, endAt });
+        await Poll.create({ id, title, options, startTime, endTime });
         res.cookie('poll', id, { httpOnly: true, secure: true, sameSite: 'strict' });
         return res.status(201).json({ message: 'Poll has been created successfully.' });
     } catch (error) {
